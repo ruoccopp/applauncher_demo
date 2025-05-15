0697ac2445637ba84506984f9eece5b3dd57c070
ruocco.p.p <ruocco.p.p@gmail.com>
2025-05-15 12:42:10 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# Modulo frontend dimostrativo - nessuna logica Python
---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "App Launcher Demo",
+    "version": "16.0.1.0",
+    "summary": "Modulo dimostrativo: banner HTML nella schermata iniziale di Odoo",
+    "category": "Interface",
+    "author": "Ruocco Pietro Paolo",
+    "website": "https://cactus.farm",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "data": [
+        "views/app_drawer_inherit.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
